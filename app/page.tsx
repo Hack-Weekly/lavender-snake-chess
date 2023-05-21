f064175e71cb38d2dd89e53ff0399ace257be8f3
Xarunoba <me@xaru.win>
2023-05-22 04:41:11 +0800

    home page changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 // import Image from "next/image";
 
 export default function Home() {
-  return <main>I am the home page.</main>;
+  return (
+    <main className="flex flex-col justify-center items-center gap-4">
+      <h2 className="text-2xl font-bold">Create Game</h2>
+      <button>Play with a Friend</button>
+      <button>Play with the Computer</button>
+      <button>Computer versus Computer</button>
+    </main>
+  );
 }
