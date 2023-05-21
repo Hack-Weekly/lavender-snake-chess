2f281c12de515186557b4697f1e4c9c14bc6831d
Xarunoba <me@xaru.win>
2023-05-22 03:21:11 +0800

    initial layout and styling

---
+++ app/_components/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <header className="flex-none">I am a header.</header>;
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="flex flex-col justify-center items-center gap-4">
+      <h2 className="font-bold text-xl">Not Found</h2>
+      <p>Could not find requested resource.</p>
+    </main>
+  );
+}
