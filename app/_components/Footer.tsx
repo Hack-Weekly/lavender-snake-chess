fa7d801ea3e50fbe03f2ceae700a9ee5ef692ed1
Xarunoba <me@xaru.win>
2023-05-22 04:50:11 +0800

    add github team link

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -4,8 +4,14 @@ export default function Footer() {
   return (
     <footer className="flex-none px-4 pb-4 flex justify-between flex-wrap">
       <p>
-        Copyright © 2023 Team Lavender Snake.
-        <span className="md:inline hidden"> All rights reserved.</span>
+        Copyright © 2023{" "}
+        <Link
+          href="https://github.com/orgs/Hack-Weekly/teams/lavender-snake"
+          target="_blank"
+          rel="nofollow noopener">
+          Team Lavender Snake
+        </Link>
+        .<span className="md:inline hidden"> All rights reserved.</span>
       </p>
       <Link
         href="https://github.com/Hack-Weekly/lavender-snake-chess"
