c5eda6b91e53e39ba32a51cd753ef3566ea362ae
Xarunoba <me@xaru.win>
2023-05-22 04:41:24 +0800

    header and footer changes

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,3 +1,18 @@
+import Link from "next/link";
+
 export default function Footer() {
-  return <footer className="flex-none">I am a footer.</footer>;
+  return (
+    <footer className="flex-none px-4 pb-4 flex justify-between flex-wrap">
+      <p>
+        Copyright © 2023 Team Lavender Snake.
+        <span className="md:inline hidden"> All rights reserved.</span>
+      </p>
+      <Link
+        href="https://github.com/Hack-Weekly/lavender-snake-chess"
+        target="_blank"
+        rel="nofollow noopener">
+        Github
+      </Link>
+    </footer>
+  );
 }
--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,3 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
-  return <header className="flex-none">I am a header.</header>;
+  return (
+    <header className="flex-none flex justify-between items-center px-4 pt-4">
+      <Link href="/" className="flex gap-2 justify-center items-center">
+        <Image src="/chess.svg" alt="Chessss Logo" width={48} height={48} />
+        <h1>Chessss</h1>
+      </Link>
+      <nav className="flex gap-4">
+        <Link href="/">Home</Link>
+        {/* TODO: Handle auth */}
+        <Link href="/profile">Profile</Link>
+      </nav>
+    </header>
+  );
 }
