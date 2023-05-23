c4f11b3999f716a088a50f7d45ee6917a7fdd53e
Xarunoba <me@xaru.win>
2023-05-24 00:40:29 +0800

    added a check if unauthenticated

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,19 @@
 // import Image from "next/image";
+"use client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Loader from "@/components/Loader";
 
 export default function Profile() {
+  const router = useRouter();
+  const { data, status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/");
+    },
+  });
+  if (status === "loading") {
+    return <Loader text="Profile" />;
+  }
   return <main>I am the profile page.</main>;
 }
