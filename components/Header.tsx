232bc1be165121732d67ede42605b0e8b9682487
Xarunoba <me@xaru.win>
2023-05-24 00:27:10 +0800

    added profile dropdown functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,9 @@
 "use client";
-import { signIn, useSession } from "next-auth/react";
+import { Menu, Transition } from "@headlessui/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { Fragment } from "react";
 
 export default function Header() {
   const { data } = useSession();
@@ -15,21 +17,60 @@ export default function Header() {
       </Link>
       <nav className="flex gap-4 justify-center items-center">
         <Link href="/">Home</Link>
-        {/* TODO: Handle auth */}
         {data && data.user.image ? (
-          <Link
-            href="/profile"
-            className="flex flex-nowrap items-center gap-1.5">
-            <Image
-              src={data.user.image}
-              alt="Profile Picture"
-              width={16}
-              height={16}
-            />
-            <span className="max-w-[7ch] overflow-hidden text-ellipsis">
-              {data.user.name ? data.user.name : "You"}
-            </span>
-          </Link>
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-full bg-white text-sm font-semibold text-gray-900 shadow-sm hover:grayscale grayscale-0 transition duration-150">
+                <Image
+                  src={data.user.image}
+                  alt="Your Profile Picture"
+                  width={32}
+                  height={32}
+                />
+              </Menu.Button>
+            </div>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95">
+              <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div>
+                  <Menu.Item disabled>
+                    {({ active }) => (
+                      <div className="flex flex-col px-4 py-2 text-sm bg-white text-gray-900">
+                        <p>Signed in as:</p>
+                        <p className="font-semibold">{data.user.name}</p>
+                      </div>
+                    )}
+                  </Menu.Item>
+                </div>
+                <div>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href="/profile"
+                        className="ui-active:bg-gray-100 ui-active:text-gray-900 ui-not-active:text-gray-700 block px-4 py-2 text-sm">
+                        Profile
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        onClick={() => signOut()}
+                        className="ui-active:bg-gray-100 ui-active:text-gray-900 ui-not-active:text-gray-700 block w-full px-4 py-2 text-left text-sm">
+                        Sign out
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
         ) : (
           <button
             onClick={() => signIn("discord")}
